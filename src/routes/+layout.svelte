e0a01e069c7b6420aa0891e51c66746a230a9653
AKurmazov <akurmazov2001@gmail.com>
2022-10-12 23:36:46 +0300

    Make pages urls be relative

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <div class="container">
     <header class="d-flex justify-content-center py-3">
     <ul class="nav nav-pills">
-        <li class="nav-item"><a href="/" class="nav-link active" aria-current="page">About</a></li>
-        <li class="nav-item"><a href="/stocks" class="nav-link">Stocks</a></li>
-        <li class="nav-item"><a href="/education" class="nav-link">Education</a></li>
-        <li class="nav-item"><a href="/experience" class="nav-link">Experience</a></li>
-        <li class="nav-item"><a href="/contacts" class="nav-link">Contacts</a></li>
+        <li class="nav-item"><a href="?" class="nav-link active" aria-current="page">About</a></li>
+        <li class="nav-item"><a href="stocks" class="nav-link">Stocks</a></li>
+        <li class="nav-item"><a href="education" class="nav-link">Education</a></li>
+        <li class="nav-item"><a href="experience" class="nav-link">Experience</a></li>
+        <li class="nav-item"><a href="contacts" class="nav-link">Contacts</a></li>
     </ul>
     </header>
 </div>
